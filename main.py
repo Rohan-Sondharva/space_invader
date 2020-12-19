653ba3cf7e7325d60c47af822d9718e5b0151fba
Rohan Sondharva <rohansonhdarva@gmail.com>
2020-12-19 14:45:01 +0530

    Added asset folder

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello')
+import pygame
+
+# Initialize the Pygame
+pygame.init()
+
+# Create the Screen
+screen = pygame.display.set_mode((800, 600))
+
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
