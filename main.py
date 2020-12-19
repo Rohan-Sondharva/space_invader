3aaa87f57501a4336f78a1239e4bf54bacfbbfa0
Rohan Sondharva <rohansonhdarva@gmail.com>
2020-12-19 17:25:27 +0530

    Added Bullet in game

--- main.py
+++ main.py
@@ -28,6 +28,16 @@ enemyY = random.randint(50, 150)
 enemyX_change = 3
 enemyY_change = 30
 
+# Adding bullet image and position
+# Ready - You can't see the Bullet
+# Fire - You can see the bullet and it is moving
+bulletImg = pygame.image.load('assets/bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 20
+bullet_state = 'ready'
+
 
 # Drawing Player to Screen
 def player(x, y):
@@ -37,6 +47,14 @@ def player(x, y):
 # Drawing enemy to Screen
 def enemy(x, y):
     screen.blit(enemyImg, (x, y))
+
+
+# Change the bullet state to Fire
+def fire_bullet(x, y):
+    # Making variable global
+    global bullet_state
+    bullet_state = 'fire'
+    screen.blit(bulletImg, (x + 16, y + 10))
 
 
 running = True
@@ -62,6 +80,8 @@ while running:
                 playerX_change = -5
             if event.key == pygame.K_RIGHT:
                 playerX_change = 5
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
 
         # Checks whether the key is released or not
         if event.type == pygame.KEYUP:
@@ -87,6 +107,11 @@ while running:
     elif enemyX >= 736:
         enemyX_change = -3
         enemyY += enemyY_change  # Move Enemy Down
+
+    # Bullet Movement
+    if bullet_state == 'fire':
+        fire_bullet(playerX, bulletY)
+        bulletY -= bulletY_change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
