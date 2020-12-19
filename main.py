42d355c291d5458ef4cf577610feffa7c3875b79
Rohan Sondharva <rohansonhdarva@gmail.com>
2020-12-19 15:51:19 +0530

    Added Basic Movements

--- main.py
+++ main.py
@@ -11,25 +11,46 @@ pygame.display.set_caption('Space Invaders')
 icon = pygame.image.load('assets/icon.png')
 pygame.display.set_icon(icon)
 
-# Adding player image
+# Adding player image and position
 playerImg = pygame.image.load('assets/player.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
+
 
 # Drawing Player to Screen
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
+
+running = True
 
 # Game Loop
-running = True
 while running:
 
+    # Fill the screen with RGB color
     screen.fill((0, 0, 0))
 
+    # Loop through all the events in game
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        # Checks whether the key is pressed or not
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+
+        # Checks whether the key is released or not
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    # Add new position to PlayerX
+    playerX += playerX_change
+    player(playerX, playerY)
+
     pygame.display.update()
